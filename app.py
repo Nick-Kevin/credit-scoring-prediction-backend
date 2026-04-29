5b9c80008f6ec488cb7fc4de0d09c91e36426864
Nick-Kevin <nicblou23@gmail.com>
2026-04-30 02:05:44 +0400

    add : endpoint for the post

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -8,5 +8,14 @@ CORS(app) # enable the react front app to access this API
 def get_data():
     return jsonify({"message": "Hello from Flask!"})
 
+@app.route('/api/endpoint', methods=['POST'])
+def handle_json():
+    data = request.get_json()
+    persoItems = data.get('persoItems')
+    return jsonify({"received": persoItems})
+
+
+# ensures the development server only starts if you run the script directly,
+# preventing it from launching accidentally if the file is imported elsewhere
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
